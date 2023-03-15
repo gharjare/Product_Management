f18e9a140b662e68938253f93e75e5ddea22b65d
gharjare <gharjarechetan@gmail.com>
2023-03-15 15:37:22 +0530

    [chetan gharjare] add 25 default values

---
+++ ProductManagement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReveiwManagement
+{
+    public class ProductManagement
+    {
+        List<ProductReview> productReviewList = new List<ProductReview>();
+
+        public void AddProductList(List<ProductReview> list)
+        {
+            this.productReviewList = list;
+        }
+    }
+}
